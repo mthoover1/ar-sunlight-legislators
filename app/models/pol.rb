118ba8a0579e0eaa9e4a9cdb42882b66cb454ed9
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-31 17:05:42 -0500

    added male scope

--- app/models/pol.rb
+++ app/models/pol.rb
@@ -1,8 +1,6 @@
 require_relative '../../db/config'
 
 class Pol < ActiveRecord::Base
-  scope :female, where('gender=?', 'F')
+  scope :female, where('gender = ?', 'F')
+  scope :male, where('gender = ?', 'M')
 end
-
-
-
